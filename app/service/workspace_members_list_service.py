248be604f18c380076341d385b9bbbc38b4bba57
Devshree <devshree0602@gmail.com>
2026-06-16 23:02:18 +0530

    Added status code changes for members list

--- app/service/workspace_members_list_service.py
+++ app/service/workspace_members_list_service.py
@@ -23,7 +23,7 @@ def get_workspace_members(workspace_id: int,db:Session=Depends(getDb)):
 
     if not workspace_exist:
         raise HTTPException(
-            status_code=100,
+            status_code=404,
             detail="Workspace does not exist"
         )
     
