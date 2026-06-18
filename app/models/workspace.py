0df0f026bf68064949b2c5806ee44fcdb1a35bf6
Devshree <devshree0602@gmail.com>
2026-06-18 22:41:47 +0530

    Added string len changes for workspace_details

--- app/models/workspace.py
+++ app/models/workspace.py
@@ -9,7 +9,7 @@ class Workspace(Base):
     __tablename__ = "workspace_details"
     workspace_id = Column(BigInteger,primary_key=True,index=True)
     workspace_name = Column(String(200))
-    workspace_description = Column(String)
+    workspace_description = Column(String(200))
     created_at = Column(DateTime,default=datetime.utcnow)
     modified_at = Column(DateTime,default=datetime.utcnow)
     created_by = Column(BigInteger,ForeignKey("user_details.id"))
